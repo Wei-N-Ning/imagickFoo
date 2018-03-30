97315d081cf2421f75696f82833efef61e6f581c
Wei Ning <nwcgnw@gmail.com>
2018-03-30 19:36:35 +1300

    - init project
    - add first example: batch resizing

---
+++ src/modification/resizing/toWidth.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+function createTestData() {
+    rm -rf /tmp/sut
+    rm -rf /tmp/resized
+    cp -rf ../../_testdata /tmp/sut
+    mkdir /tmp/resized
+}
+
+function doResize() {
+    mogrify -path /tmp/resized -resize 60x60% -quality 80 -format jpg /tmp/sut/*
+}
+
+createTestData
+doResize
+
+ls -l /tmp/sut
+ls -l /tmp/resized
